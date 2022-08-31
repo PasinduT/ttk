045682cfae5123e162e104920963bf9579d2f16f
Pasindu Tennakoon <tennakoonpasindu@gmail.com>
2022-08-30 18:19:04 -0700

    Started CVT

---
+++ core/base/combinatorialVectorField/CombinatorialVectorField.cpp
@@ -0,0 +1,6 @@
+#include <CombinatorialVectorField.h>
+
+ttk::CombinatorialVectorField::CombinatorialVectorField() {
+  // inherited from Debug: prefix will be printed at the beginning of every msg
+  this->setDebugMsgPrefix("CombinatorialVectorField");
+}
